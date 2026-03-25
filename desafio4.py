f0ebc10188b71c2f3f69f4c97b33e59c6d4dbbd4
brendajobins-pixel <brenda.jobins@escola.pr.gov.br>
2026-03-25 08:50:45 -0300

    Create desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,17 @@
+#4CALCULADORA
+n1 = float(input("selecione um número: "))
+n2 = float(input("selecione outro número: "))
+opereração = input("escolha uma operação [subtração, soma, multiplicação, divisão]: ")
+if operação == "subtração":
+   print( n1 + n2)
+elif opereção == "soma":
+   print( n1 - n2)
+elif operação == "multiplicação":
+   print( n1 * n2)
+elif operação == "divisão":
+   print( n1 / n2)
+  E
+  
+
+
+
