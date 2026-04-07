72c901a4e23cd418d5184ccd088f1fc3cba62d4b
brendajobins-pixel <brenda.jobins@escola.pr.gov.br>
2026-04-07 09:43:51 -0300

    Create desafio6.py

---
+++ desafio6.py
@@ -0,0 +1,10 @@
+#6 tempo
+segundos = int(input("Digite a quantidade de segundos: "))
+
+horas = segundos // 3600
+resto = segundos % 3600
+
+minutos = resto // 60
+segundos_restantes = resto % 60
+
+print("Horas:
