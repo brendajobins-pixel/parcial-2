53d4fc1a925e00b224f2fd60a80352000a3a8d61
brendajobins-pixel <brenda.jobins@escola.pr.gov.br>
2026-04-07 09:31:51 -0300

    Create desafio5.py

---
+++ desafio5.py
@@ -0,0 +1,5 @@
+5#cacular a área de um triangulo
+base = float(input("qual a base do seu triangulo:"))
+altura = float(input("qual a altura de seu triangulo:"))
+area = (base * altura) / 2
+print(area)
