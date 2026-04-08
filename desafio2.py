c46ae796218008028522d19e3c2f8e832dbb3f65
brendajobins-pixel <brenda.jobins@escola.pr.gov.br>
2026-04-08 20:18:50 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,6 +1,15 @@
-#2 confirma se o número é par
-número = int(input("escolha um número aleatóriio: "))
-if número % 2 == 0:
-   print(" ele é par")
+# ===== DESAFIO 02 - VERIFICANDO SE O NÚMERO É PAR =====
+
+# Exibe um título na tela
+print('===== DESAFIO 02 - VERIFICANDO SE O NÚMERO É PAR =====')
+
+# Solicita que o usuário digite um número inteiro
+numero = int(input('Escolha um número aleatório: '))
+
+# Verifica se o número é par
+# Um número é par quando o resto da divisão por 2 é igual a 0
+if numero % 2 == 0:
+    print('O número é par')
 else:
-   print("ele é ímpar")
+    # Caso não seja par, automaticamente é ímpar
+    print('O número é ímpar')
