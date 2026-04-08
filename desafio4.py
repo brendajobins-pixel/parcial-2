6abd441f15308e662d380a1e2c82f232a150389e
brendajobins-pixel <brenda.jobins@escola.pr.gov.br>
2026-04-08 20:19:48 -0300

    Update desafio4.py

--- desafio4.py
+++ desafio4.py
@@ -1,18 +1,37 @@
-#4CALCULADORA
-n1 = float(entrada("selecione um número: "))
-n2 = float(entrada("selecione outro número: "))
-opereração = entrada("escolha uma operação [subtração, soma, multiplicação, divisão]: ")
-se operação == "soma":
-   print( n1 + n2)
-elif opereção == "soma":
-   print( n1 - n2)
-elif operação == "multiplicação":
-   print( n1 * n2)
-elif operação == "divisão":
-   print( n1 / n2)
-outro:
-  print("essa operação não existe")
-  
+# ===== DESAFIO 04 - CALCULADORA =====
+
+# Exibe um título na tela
+print('===== DESAFIO 04 - CALCULADORA =====')
+
+# Solicita que o usuário digite o primeiro número
+n1 = float(input('Selecione um número: '))
+
+# Solicita que o usuário digite o segundo número
+n2 = float(input('Selecione outro número: '))
+
+# Solicita a operação desejada
+operacao = input('Escolha uma operação [soma, subtração, multiplicação, divisão]: ')
+
+# Verifica qual operação foi escolhida
+if operacao == "soma":
+    print('Resultado:', n1 + n2)
+
+elif operacao == "subtração":
+    print('Resultado:', n1 - n2)
+
+elif operacao == "multiplicação":
+    print('Resultado:', n1 * n2)
+
+elif operacao == "divisão":
+    # Verifica se o segundo número é zero (evita erro)
+    if n2 != 0:
+        print('Resultado:', n1 / n2)
+    else:
+        print('Erro: divisão por zero não é permitida')
+
+else:
+    # Caso a operação não exista
+    print('Essa operação não existe')
 
 
 
